2fb8409d4dc19675b443a957d10d8ee1bd63e42a
Mitchell C Murphy <mcmwriter@gmail.com>
2016-03-27 11:20:09 -0700

    Hacky fix for mobile menu (will need a better fix with css) and added padding to body when mobile so there is no content lined up right against the browser window

--- js/menu.js
+++ js/menu.js
@@ -2,28 +2,19 @@ function animateMenuIcon(x) {
     x.classList.toggle("change");
     
 	$( ".navbar-full" ).slideToggle( "slow", function() {
-		if ($(this).is(':visible'))
+		if ($(this).is(':visible')){
 			$(this).css('display','inline-block');
+			$(document).bind('scroll',function () { 
+			       window.scrollTo(0,0); 
+			  });
+		}
+		else{
+			$(document).unbind('scroll');
+		}
+
 	});
-    /* if($('#menuIcon').hasClass("change")){
-    	
-	    $( ".navbar-full" ).slideDown( "slow", function() {
-	    	
-		});
-	}
-	else{
-		$( ".navbar-full" ).slideUp( "slow", function() {
-		    
-		});
-	} */
 }
 
-// $(".dropdown").hover(function() {
-//     $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-//    },
-//    function() {
-//      $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-// });
 
 $(window).scroll(function () {
     if ($(window).scrollTop() > 45) {
