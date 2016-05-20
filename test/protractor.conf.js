a7070f429631e64901ae8fbda32c75fa30bf1373
Mitchell C. Murphy <mcmwriter@gmail.com>
2016-05-19 20:40:42 -0700

    Removed unwanted code

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -3,10 +3,7 @@ exports.config = {
   seleniumServerJar: '/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar',
   seleniumPort: null,
   chromeDriver: '/usr/local/lib/node_modules/protractor/selenium/chromedriver_2.21',
-  specs: ['e2e-spec/main.js',
-          'e2e-spec/login.js',
-          'e2e-spec/packages.js',
-          'e2e-spec/modal.js'],
+  specs: ['e2e-spec/*.js'],
   jasmineNodeOpts: {
     // If true, display spec names.
     isVerbose: true,
